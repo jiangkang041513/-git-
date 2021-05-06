962aa3d4950e200747f40f07de0cf03e5dabb503
Jack <1332602795@qq.com>
2021-05-06 11:13:07 +0800

    第二次提交

--- git测试.c
+++ git测试.c
@@ -3,24 +3,27 @@
 #include <string.h>
 
 #define MAX_LINE 1024
+
 void main (int argc, char* argv[])
 {
-    char file_name[100]="F:\\file.txt";
+	char file_name[100]="./file.txt";
 	char buffer[MAX_LINE] ;
 	int len = 0; //行字符个数
 	int total = 0;
 	int sum = 0;
     FILE *fp;
+	if((fp=fopen(argv[2],"r"))==NULL){  
+        	printf("没有该文件\n\n\n");
+	}
     int i;
-    fp=fopen (file_name, "r") ; //"w+"模式：先写入后读出
+    /*fp=fopen (file_name, "r") ; 
     if(NULL==fp)
     {
         printf ("Failed to open the file !\n");
         exit (0);
-    }
+    }*/
 	//读取文件内容
 	while(! feof(fp)){
-
 		// fgets() 在读出 MAX_LINE 个字符之前，遇到 换行符 或 EOF 则读出结束
 		if(fgets(buffer, MAX_LINE, fp) != NULL){
 			len = strlen(buffer);
